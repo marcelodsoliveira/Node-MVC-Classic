1a243eef7f944d566a63b981f3f2745ce6b349f6
Marcelo Santos <oliveira.lnx@gmail.com>
2025-05-26 13:17:23 -0300

    Test in MVC based project

---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+import { create } from 'domain';
+import express from 'express';
+import { createContactController, deleteContactController, getContactsController } from '../controller/contact';
+
+
+const router = express.Router();
+
+router.post('/contato', createContactController);
+router.get('/contatos', getContactsController);
+router.delete('/contato', deleteContactController);
+
+export default router;
